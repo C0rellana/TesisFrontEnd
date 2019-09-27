fe3fe4c02e345eec61f949e6f4a01a951984ebdf
carloxmof1 <carlosmof15@gmail.com>
2019-09-27 12:43:24 -0300

    verificar rut

--- src/components/Auth/Registro.jsx
+++ src/components/Auth/Registro.jsx
@@ -67,6 +67,53 @@ class Register extends React.Component {
     });
   }
 
+  checkRut(rut) {
+    
+    // Despejar Puntos
+    var valor = rut.value.replace('.','');
+    // Despejar Guión
+    valor = valor.replace('-','');
+    
+    // Aislar Cuerpo y Dígito Verificador
+    var cuerpo = valor.slice(0,-1);
+    var dv = valor.slice(-1).toUpperCase();
+    
+    // Formatear RUN
+    rut.value = cuerpo + '-'+ dv
+    
+    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+    
+    // Calcular Dígito Verificador
+    var suma = 0;
+    var multiplo = 2;
+    var index;
+    var dvEsperado;
+    // Para cada dígito del Cuerpo
+    for(var i=1;i<=cuerpo.length;i++) {
+    
+        // Obtener su Producto con el Múltiplo Correspondiente
+        index = multiplo * valor.charAt(cuerpo.length - i);
+        
+        // Sumar al Contador General
+        suma = suma + index;
+        
+        // Consolidar Múltiplo dentro del rango [2,7]
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+  
+    }
+    
+     // Calcular Dígito Verificador en base al Módulo 11
+     dvEsperado = 11 - (suma % 11);    
+     // Casos Especiales (0 y K)
+     dv = (dv === 'K')?10:dv;
+     dv = (dv === 0)?11:dv; 
+     // Validar que el Cuerpo coincide con su Dígito Verificador
+     if(dvEsperado !== parseInt(dv)) { rut.setCustomValidity("RUT Inválido"); return false; }
+     
+     // Si todo sale bien, eliminar errores (decretar que es válido)
+     rut.setCustomValidity('');
+}
 
   async handleSubmit(event) {
     event.preventDefault();
@@ -141,10 +188,13 @@ class Register extends React.Component {
                                 <i className="ni ni-hat-3" />
                                 </InputGroupText>
                             </InputGroupAddon>
-                            <Input  name="rut" type="text"  placeholder="Rut: 12345678" onChange={this.handleInputChange} required/>
+                            <Input  name="rut" type="text"  
+                            onInput={e=>this.checkRut(e.target)}
+                            placeholder="Ingrese rut sin puntos"
+                             onChange={this.handleInputChange} required/>
                             </InputGroup>
                         </FormGroup>    
-                                        {/*  pattern="[0-9]{7,8}" */}
+                                       
                     
                         <FormGroup >
                             <InputGroup className=" mb-3">
